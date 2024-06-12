742583d1c63900d5f17fa457f2fa3af4f2a05c9e
Italo Valcy <italo@ampath.net>
2024-06-12 14:50:16 -0300

    refactor utils.get_timestamp() to simplify and make it comply with Topology Data Model Spec 2.0 -- timestamp must be UTC

--- utils.py
+++ utils.py
@@ -1,12 +1,6 @@
 """SDX topology Utility functions"""
-from datetime import datetime
-import pytz
+from datetime import datetime, timezone
 
-
-def get_timestamp(timestamp=None):
-    """Function to obtain the current time_stamp in a specific format"""
-    if timestamp is not None:
-        if len(timestamp) >= 19:
-            return timestamp[:10]+"T"+timestamp[11:19]+"Z"
-    return datetime.now(
-            pytz.timezone("America/New_York")).strftime("%Y-%m-%dT%H:%M:%SZ")
+def get_timestamp():
+    """Return the current datetime in UTC formatted as string"""
+    return datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
