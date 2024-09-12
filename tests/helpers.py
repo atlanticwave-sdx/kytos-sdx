8a0f3a658000c50f509a85f03cb2e62c6d97560c
Italo Valcy <italo@ampath.net>
2024-09-12 10:08:45 -0300

    removing unnecessary comments

--- tests/helpers.py
+++ tests/helpers.py
@@ -20,23 +20,15 @@ def get_topology():
     for key, value in topo["topology"]["switches"].items():
         switch = Switch(key)
         switch.enable()
-        #switch.id = key
         switch.is_active = MagicMock(return_value = value["active"])
-        #switch.is_enabled.return_value = value["enabled"]
         switch.metadata = value["metadata"]
         switch.description["data_path"] = value["data_path"]
-        #switch.as_dict.return_value = value
-        #switch.interfaces = {}
         switches[key] = switch
 
         for intf_id, intf in value["interfaces"].items():
             interface = Interface(intf["name"], intf["port_number"], switch, speed=intf["speed"])
             interface.enable()
-            #interface.id = intf_id
-            #interface.is_active.return_value = intf["active"]
-            #interface.is_enabled.return_value = intf["enabled"]
             interface.metadata = intf["metadata"]
-            #interface.as_dict.return_value = intf
             switch.interfaces[intf_id] = interface
             interfaces[intf_id] = interface
 
@@ -45,15 +37,11 @@ def get_topology():
         intf2 = interfaces[value["endpoint_b"]["id"]]
         link = Link(intf1, intf2)
         link.enable()
-        #link.id = key
-        #link.is_active.return_value = value["active"]
-        #link.is_enabled.return_value = value["enabled"]
         link.metadata = value["metadata"]
         intf1.update_link(link)
         intf2.update_link(link)
         intf1.nni = True
         intf2.nni = True
-        #link.as_dict.return_value = value
         links[key] = link
 
     topology = MagicMock()
